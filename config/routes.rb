fd3bb98481065e32b8e0cbd0b44e57734b672dc4
LizHuang <search.psop@gmail.com.tw>
2016-05-04 22:02:23 +0800

    user 可以在 group 頁面 加入 / 退出 此 group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
+
     resources :posts
   end
   # The priority is based upon order of creation: first created -> highest priority.
