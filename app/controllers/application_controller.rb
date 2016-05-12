b28b412c2cac4a4f4eb153fa4d78c3cea69d8131
LizHuang <search.psop@gmail.com.tw>
2016-05-12 23:50:52 +0800

    解決不能註冊會員的問題

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::Base
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up) do |u|
         u.permit(:name, :email, :password, :password_confirmation)
-     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
      end
    end
 end
